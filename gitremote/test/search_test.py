58d0501b9689dd162c48f9cd3a9bdd37191738e0
LucasCollins <ldcollins@my.waketech.edu>
2021-11-07 13:41:54 -0500

    Added pytest

--- gitremote/test/search_test.py
+++ gitremote/test/search_test.py
@@ -1,18 +1,22 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 
-@pytest.mark.parametrize("search_term", "wake tech")
 
+@pytest.mark.parametrize("search_term", "wake tech")
 def test_wiki_search(search_term):
-    with webdriver.Chrome() as driver:
+    with webdriver.Chrome(executable_path='C:/ChromeDriver/bin/chromedriver.exe') as driver:
         wait = WebDriverWait(driver, 15)
 
         driver.get('https://en.wikipedia.org')
-        driver.find_element(By.NAME, "search").send_keys(search_term + Keys.Return)
+        driver.find_element(By.NAME, "search").send_keys(search_term)
+        driver.find_element(By.NAME, "go").click()
 
-        result = wait.until(presence_of_element_located(By.CSS_SELECTOR, 'h1'))
+        result = wait.until(presence_of_element_located((By.CLASS_NAME, "firstHeading")))
+        print(result.get_attribute('textContent'))
         assert result.get_attribute('textContent') == 'Wake Technical Community College'
+
+
+test_wiki_search('wake tech')
